23145926ea90160d96f4d9528fe5cd909ec5fe76
Phuoc Nguyen Pham <130674444+pn-pham@users.noreply.github.com>
2024-10-26 16:14:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 from urllib.request import Request, urlopen
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
+  url = request.args.get('url') if request.method == 'GET' else request.form.get('url')
   req = Request(
-      url='https://www.livesoccertv.com/',
+      url=url,
       headers={'User-Agent': 'Mozilla/5.0'}
   )
   webpage = urlopen(req).read()
-  # print(webpage)
   return webpage
